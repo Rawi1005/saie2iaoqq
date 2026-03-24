72ff1447178ec8ac2eebe69a81e022e5da8950f6
Rawi1005 <rawinakasem@gmail.com>
2026-03-24 23:20:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 )
 
 API_KEY = os.getenv("OPENAI_API_KEY")
-LEGACY_API_KEY = os.getenv("API_KEY", "Sìshísìsìshísìsì")
+LEGACY_API_KEY = os.getenv("API_KEY", "yiersansi")
 VALID_API_KEYS = {k for k in [API_KEY, LEGACY_API_KEY] if k}
 PIXELDRAIN_API_KEY = os.getenv("PIXELDRAIN_API_KEY", "01fc925f-6884-4b46-b16d-0c53b6b0c12c")
 
